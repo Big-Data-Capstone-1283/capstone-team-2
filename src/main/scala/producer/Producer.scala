c58044631d59fe117e8464c4bf40312d98850340
AlexWhite252 <alexanderwhite252@gmail.com>
2022-04-21 14:54:49 -0500

    Changed topic

--- src/main/scala/producer/Producer.scala
+++ src/main/scala/producer/Producer.scala
@@ -3,7 +3,7 @@ package producer
 import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 
-class Producer {
+class Producer extends App{
   val props:Properties = new Properties()
   //Replace ip with your ip from server.config
   props.put("bootstrap.servers","ec2-3-93-174-172.compute-1.amazonaws.com:9092")
@@ -13,8 +13,7 @@ class Producer {
     "org.apache.kafka.common.serialization.StringSerializer")
   props.put("acks","all")
   val producer = new KafkaProducer[String, String](props)
-  //Update topic when they create theirs
-  val topic = "team1"
+  val topic = "team2"
 
 
 }
