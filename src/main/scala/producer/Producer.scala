04158edd7c64ee0944677bb51187d70dbb41a610
Brandon Cho <100604806+BrandonYCho@users.noreply.github.com>
2022-04-22 09:58:48 -0700

    Added placeholder Producer

--- src/main/scala/producer/Producer.scala
+++ src/main/scala/producer/Producer.scala
@@ -2,6 +2,7 @@ package producer
 
 import java.util.Properties
 import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
+import producer.Orders._
 
 class Producer extends App{
   val props:Properties = new Properties()
@@ -15,5 +16,28 @@ class Producer extends App{
   val producer = new KafkaProducer[String, String](props)
   val topic = "team2"
 
-
+  try {
+    // for loop determines number of batches
+    for (i <- 1 to 2) {
+      val batch = createOrders() // placeholder for data generation method
+      println(s"Sending batch #$i ")
+      Thread.sleep(2000) // for testing purposes, to give user time to read
+      batch.foreach(x => {
+        // REMINDER: change substring so that it pulls OrderID (I don't know how long it is currently)
+        val record = new ProducerRecord[String, String](topic, x.substring(0, 10).toString, x)
+        val metadata = producer.send(record)
+        // Display to console what is being sent to Kafka broker
+        printf(s"Sent record(key=%s value=%s) " +
+          "meta(partition=%d, offset=%d)\n",
+          record.key(), record.value(),
+          metadata.get().partition(),
+          metadata.get().offset())
+      })
+      producer.flush()
+    }
+  } catch {
+    case e: Exception =>  e.printStackTrace()
+  } finally {
+    producer.close()
+  }
 }
