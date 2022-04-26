afa34096d6dc4abff3290eba64d3e5954ddea607
Jeffrey Hafner <jeffrey547@revature.net>
2022-04-26 11:20:20 -0500

    Added WeightedRandomizer ability to use Integers

--- src/main/scala/producer/WeightedRandomizer.scala
+++ src/main/scala/producer/WeightedRandomizer.scala
@@ -1,6 +1,6 @@
 package producer
 
-object WeightedRandomizer {
+object WeightedRandomizer_Test {
 	/**
 	  * Returns a random key from a Map containing `"item name" -> weight` pairs, where the
 	  * "weight" value indicates its frequency relative to the total value of all weights.
@@ -10,8 +10,7 @@ object WeightedRandomizer {
 	  * @param items	A Map of `String -> Int` pairs, representing item name and weight.
 	  * @return			A semi-randomly chosen item name from the `items` map passed to the function.
 	  */
-
-	def apply(items: Map[String, Int]): String = {
+	def WeightedRandomizer(items: Map[String, Int]): String = {
 		var weightSum = 0  // The total value of all the weights passed in (Note: does NOT need to total to 100)
 		for (i <- items)  // Find the total value of weights for all keys
 			weightSum += i._2
@@ -19,7 +18,31 @@ object WeightedRandomizer {
 		val keys = items.keySet.toArray  // Get the key names from the map
 		var n = 0  // Current item
 		var weightTotal = 0  // Current total of all item's weights checked so far
-		while (weightTotal + items(keys(n)) < rnd) {  // Find the item for the random value's weight group
+		while (weightTotal + items(keys(n)) <= rnd) {  // Find the item for the random value's weight group
+			weightTotal += items(keys(n))
+			n += 1
+		}
+		keys(n)  // Return the semi-randomly chosen item name
+	}
+
+	/**
+	  * Returns a random key from a Map containing `item_number -> weight` pairs, where the
+	  * "weight" value indicates its frequency relative to the total value of all weights.
+	  * e.g. `WeightedRandomizer(Map(1 -> 60, 2 -> 30, 3 -> 10)` would return 1 roughly
+	  * 60% of the time, 2 ~30% of the time, and 3 ~10% of the time.
+	  *
+	  * @param items	A Map of `Int -> Int` pairs, representing item name and weight.
+	  * @return			A semi-randomly chosen item value from the `items` map passed to the function.
+	  */
+	def WeightedRandomizer(items: Map[Int, Int]): Int = {
+		var weightSum = 0  // The total value of all the weights passed in (Note: does NOT need to total to 100)
+		for (i <- items)  // Find the total value of weights for all keys
+			weightSum += i._2
+		val rnd = scala.util.Random.nextInt(weightSum)  // Pick a random number within the weight range
+		val keys = items.keySet.toArray  // Get the key names from the map
+		var n = 0  // Current item
+		var weightTotal = 0  // Current total of all item's weights checked so far
+		while (weightTotal + items(keys(n)) <= rnd) {  // Find the item for the random value's weight group
 			weightTotal += items(keys(n))
 			n += 1
 		}
