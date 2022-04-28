87a9fe36d6da9457e59511ba09979ca2daa41e67
Douglas-Lam <100620176+Douglas-Lam@users.noreply.github.com>
2022-04-28 07:42:09 -0700

    Update Consumer.scala
    change console writestream to use continous trigger

--- src/main/scala/consumer/Consumer.scala
+++ src/main/scala/consumer/Consumer.scala
@@ -2,20 +2,22 @@ package consumer
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.streaming.Trigger.ProcessingTime
+import org.apache.spark.sql.streaming.Trigger.{ProcessingTime,Continuous}
 
 object Consumer extends App {
 
-  //INITIATE SPARK SESSION//
+  //Suppress logging
   Logger.getLogger("org").setLevel(Level.ERROR)
-
+  //INITIATE SPARK SESSION//
   val spark = SparkSession
     .builder
     .appName("Kafka Streaming")
-    .config("spark.master", "local")
+    .config("spark.master", "local[4]")
+    .config("spark.streaming.stopGracefullyOnShutdown","true")
+    .config("spark.sql.shuffle.partitions",3)
     .enableHiveSupport()
     .getOrCreate()
-
+  //suppress logging
   Logger.getLogger("org").setLevel(Level.ERROR)
   spark.sparkContext.setLogLevel("ERROR")
   println("Created Spark Session")
@@ -37,11 +39,15 @@ object Consumer extends App {
     //change bootstrap server to below to reach EC2
     // ec2-3-93-174-172.compute-1.amazonaws.com:9092
     // 3.93.174.172
+    //172.25.145.45:9092
+    //Point to Amazon EC2 Zookeeper/Broker
     .option("kafka.bootstrap.servers", "ec2-3-93-174-172.compute-1.amazonaws.com:9092")
-    //Subscribe to the other team topic
+    //Subscribe to the other team's topic
     .option("subscribe", "team1")
     //startingOffsets "earliest" returns no key value table at all
-    .option("startingOffsets", "latest") // From starting
+    //latest
+    //earliest works for team2 data
+    .option("startingOffsets", "earliest") // From starting
     .option("failOnDataLoss", false)
     .load()
 
@@ -50,14 +56,22 @@ object Consumer extends App {
 
   //Since the value is in binary, first we need to convert the binary value to String using selectExpr()
   val topicStringDF = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-
-  topicStringDF.writeStream
+  val justValue = df.selectExpr("CAST(value AS STRING)")
+  //need to strip "[]" square brackets
+  //use stripPrefix/stripSuffix
+  justValue.writeStream
     .format("console")
     .outputMode("append")
-    .option("truncate", false)
+    //.option("checkpointLocation", "testCheckpoint/")
+    //.trigger(ProcessingTime("2 seconds"))
+    .trigger(Continuous("2 second"))
+    //.option("truncate", false)
     .start()
+    //.awaitTermination()
 
 
+//UNCOMMENT TO SAVE TO CSV
+    //Convert DF to csv
   topicStringDF.writeStream
     .format("csv")
     .trigger(ProcessingTime("2 seconds"))
